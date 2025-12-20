8239699868da62a1617b214592691865497528c5
RezoualiRaouf <raoufrezouali7@gmail.com>
2025-12-20 17:30:48 +0100

    rm :side bar hiding

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,6 @@
 const sideBar = document.querySelector(".dropDown-btn");
 const submenu = document.querySelector(".sub-menu");
 
-console.log(sideBar);
-console.log(submenu);
-
 sideBar.addEventListener("click", () => {
   submenu.classList.toggle("show");
 });
