93ceb894076d942235545dfe289e1ea97e8feda2
RezoualiRaouf <raoufrezouali7@gmail.com>
2025-12-21 11:34:47 +0100

    add : animation close/open drop menu add hide show side bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
-const sideBar = document.querySelector(".dropDown-btn");
+const sideBar = document.getElementById("sideBar");
+const sideBarArrow = document.querySelector(".sideBar-toggle-btn");
+const dropdownBtn = document.querySelector(".dropDown-btn");
 const submenu = document.querySelector(".sub-menu");
+const submenuArrow = document.querySelector(".dropDone-arrow");
 
-sideBar.addEventListener("click", () => {
+dropdownBtn.addEventListener("click", () => {
   submenu.classList.toggle("show");
+  submenuArrow.classList.toggle("rotate");
+});
+
+sideBarArrow.addEventListener("click", () => {
+  sideBar.classList.toggle("show");
+  sideBarArrow.classList.toggle("rotate");
 });
